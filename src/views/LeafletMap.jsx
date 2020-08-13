7690f4f302793d13134b0410fc58f54b9b1ba20f
nontage24 <nontage24@gmail.com>
2020-08-13 07:42:38 +0700

    edit marker Leaflet Map

--- src/views/LeafletMap.jsx
+++ src/views/LeafletMap.jsx
@@ -15,6 +15,20 @@ L.Icon.Default.mergeOptions({
 });
 
 class LeafletMap extends Component {
+    // For on click get LatLon
+      constructor(props) {
+        super(props);
+        this.state = {
+          currentPos: null
+        };
+        this.handleClick = this.handleClick.bind(this);
+      }    
+    
+      handleClick(e){
+        this.setState({ currentPos: e.latlng });
+      }
+    //-------------------------
+
     render() {
         return (
             <div className="content">
@@ -28,12 +42,19 @@ class LeafletMap extends Component {
                                         center={ktposition} 
                                         zoom={13} 
                                         style={{ width: '100%', height: '580px' }}
+                                        onClick={this.handleClick}
                                     >
                                         <TileLayer 
                                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                                             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                                         >             
                                         </TileLayer>
+                                        { this.state.currentPos && <Marker position={[7.9526603, 99.1545900]} draggable = {true}>
+                                                                                  
+                                            <Popup position={this.state.currentPos}>
+                                                Current location: <pre>{JSON.stringify(this.state.currentPos, null, 2)}</pre>
+                                            </Popup>
+                                        </Marker>}
 
                                         <Marker position={[7.9525503, 99.1541887]} draggable = {true}  >
                                             <Popup>draggable Marker<br />Marker can move</Popup>
